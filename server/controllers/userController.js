80a67b6d6fd9cc11e7fd95b07c9078bdf6f6f449
sohamtembhurne <sohamatembhurne@outlook.com>
2024-02-07 10:31:55 +0530

    handle errors

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,43 +2,54 @@ const { User } = require("../models/userModel");
 const { generateToken } = require("../utils/secretToken");
 
 const getUsers = async (req, res) => {
-    const allUsers = await User.find();
-
-    res.send(allUsers);
+    try {
+        const allUsers = await User.find();
+        res.send(allUsers);
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 }
 
 const createUser = async (req, res) => {
-    const { name, email, password } = req.body;
+    try {
+        const { name, email, password } = req.body;
 
-    const existingUser = await User.findOne({ email })
-    if (existingUser) {
-        return res.json({ message: 'User already exists' });
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.json({ message: 'User already exists' });
+        }
+
+        const newUser = await User.create({
+            name, email, password
+        })
+
+        const token = generateToken(newUser._id, newUser.email);
+        res.status(201).json({ message: "New user created successfully", success: true, token, newUser });
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
     }
-
-    const newUser = await User.create({
-        name, email, password
-    })
-
-    const token = generateToken(newUser._id, newUser.email);
-    res.status(201).json({ message: "New user created successfully", success: true, token, newUser });
 }
 
 const loginUser = async (req, res) => {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+        const user = await User.findOne({ email });
 
-    if (!user) {
+        if (!user) {
+            return res.json({ message: "Wrong credentials" })
+        }
+
+        if (password === user.password) {
+            const token = generateToken(user._id, user.email);
+
+            return res.status(200).json({ message: "User logged in successfully", token, success: true });
+        }
+
         return res.json({ message: "Wrong credentials" })
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" });
     }
-
-    if (password === user.password) {
-        const token = generateToken(user._id, user.email);
-
-        return res.status(200).json({ message: "User logged in successfully", token, success: true });
-    }
-
-    return res.json({ message: "Wrong credentials" })
 }
 
 module.exports = { getUsers, createUser, loginUser };
