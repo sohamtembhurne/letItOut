8718c3dafac1c793f3e669883db25e9fff8dcae1
sohamtembhurne <sohamatembhurne@outlook.com>
2024-02-07 10:46:43 +0530

    add basic tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,3 +1,15 @@
 const request = require('supertest')
 const app = require('../server')
 
+describe('Test server routes', () => {
+    it('should respond with status 200 for GET /', async () => {
+      const response = await request(app).get('/user');
+      expect(response.status).toBe(200);
+    });
+  
+    it('should respond with status 200 for POST /user/login', async () => {
+      const credentials = { email: 'test@example.com', password: 'password123' };
+      const response = await request(app).post('/user/login').send(credentials);
+      expect(response.status).toBe(200);
+    });
+  });
